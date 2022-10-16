e4dd43a4d386503de177bcc8ecf94e43711bc47b
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2022-10-16 18:19:11 +0600

    implement filter

--- InstaFilter/ViewController.swift
+++ InstaFilter/ViewController.swift
@@ -5,17 +5,35 @@
 //  Created by Shah Md Imran Hossain on 15/10/22.
 //
 
+import CoreImage
 import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var intensity: UISlider!
+    var currentImage: UIImage!
+    var filterNames = [String]()
+    
+    // CIConext - Core Image Context
+    // This is a Core image component which handles rendering
+    // we will create it once and use it throughout our app with it's property
+    // because creating context is computationally expensive
+    var context: CIContext!
+
+    // CIFilter - Core Image Filter
+    // store user activated filter
+    var currentFilter: CIFilter!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        filterNames = ["CISepiaTone", "CIGaussianBlur", "CIBumpDistortion", "CITwirlDistortion", "CIPixellate", "CISharpenLuminance", "CIUnsharpMask", "CIVignette", "CIColorPolynomial", "CIDarkenBlendMode"]
+        
         title = "Insta Filter"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+        context = CIContext()
+        // CISepiaTone is a core image filter name
+        currentFilter = CIFilter(name: filterNames[0])
     }
     
     @objc func importPicture() {
@@ -25,13 +43,100 @@ class ViewController: UIViewController {
         present(picker, animated: true)
     }
 
-    @IBAction func changeFilter(_ sender: Any) {
+    @IBAction func changeFilter(_ sender: UIButton) {
+        let ac = UIAlertController(title: "Choose filter", message: nil, preferredStyle: .actionSheet)
+        
+//        for name in filterNames {
+//            ac.addAction(UIAlertAction(title: name, style: .default, handler: setFilter))
+//        }
+        
+        ac.addAction(UIAlertAction(title: "CIGaussianBlur", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIBumpDistortion", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIPixellate", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CISepiaTone", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CITwirlDistortion", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CISharpenLuminance", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIUnsharpMask", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIVignette", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIColorPolynomial", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIDarkenBlendMode", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "cancel", style: .cancel))
+        
+        if let popoverController = ac.popoverPresentationController {
+            popoverController.sourceView = sender
+            popoverController.sourceRect = sender.bounds
+        }
+        
+        present(ac, animated: true)
+    }
+    
+    func setFilter(action: UIAlertAction) {
+        guard currentImage != nil else {
+            print("current image is nil")
+            return
+        }
+        
+        guard let actionTitle = action.title else {
+            print("action title is nil")
+            return
+        }
+        
+        currentFilter = CIFilter(name: actionTitle)
+        
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
     }
     
     @IBAction func save(_ sender: Any) {
+        guard let imageContent = imageView.image else {
+            print("Image is nil")
+            return
+        }
+        
+        UIImageWriteToSavedPhotosAlbum(imageContent, self, #selector(image(_: didFinishSavingWithError:contextInfo:)), nil)
     }
     
     @IBAction func intensityChanged(_ sender: Any) {
+        applyProcessing()
+    }
+    
+    func applyProcessing() {
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            // input intensity key for intensity value changed
+            currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
+        }
+        
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey)
+        }
+        
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey)
+        }
+        
+        if inputKeys.contains(kCIInputCenterKey) {
+            currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey)
+        }
+        
+        guard let outputImage = currentFilter.outputImage else {
+            print("output image is nil")
+            return
+        }
+        
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {
+            print("creating cgImage failed")
+            return
+        }
+        
+        let processedImage = UIImage(cgImage: cgImage)
+        imageView.image = processedImage
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError: Error?, contextInfo: UnsafeRawPointer) {
+        
     }
 }
 
@@ -45,6 +150,12 @@ extension ViewController: UIImagePickerControllerDelegate {
         
         dismiss(animated: true)
         currentImage = image
+        
+        let beginImage = CIImage(image: currentImage)
+        // input image key
+        // when inputing image for filter
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
     }
 }
 
